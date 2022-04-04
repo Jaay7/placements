57df45a14dfb44729fc338d02597e6fd8ca96253
Jaay7 <jayakrishna.madem@gmail.com>
2022-04-04 08:54:00 +0530

    query for student details to display

--- src/components/dashboard/StudentDetails.jsx
+++ src/components/dashboard/StudentDetails.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useQuery, gql } from "@apollo/client";
+import { Typography } from '@mui/material';
+
+const get_student_details = gql`
+query StudentDetails {
+  studentDetails {
+    education
+    currentCgpa
+    experience
+    skills
+  }
+}
+`
 
 const StudentDetails = () => {
+
+  const { data, loading, error } = useQuery(get_student_details, {
+    context: {
+      headers: {
+        authorization: 'JWT ' + localStorage.getItem('token')
+      }
+    },
+    pollInterval: 500
+  });
+
   return (
-    <div>StudentDetails</div>
+    loading ? <div>Loading...</div> :
+    error ? <div>Error! {error.message}</div> :
+    <div>
+      <Typography>{data.studentDetails.education}</Typography>
+      <Typography>{data.studentDetails.currentCgpa}</Typography>
+      <Typography>{data.studentDetails.experience}</Typography>
+      <Typography>{data.studentDetails.skills}</Typography>
+    </div>
   )
 }
 
