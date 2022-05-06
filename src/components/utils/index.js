91c074e3a8af8e0ce4d513e6da10fd6bd8b41bfa
Jaay7 <jayakrishna.madem@gmail.com>
2022-05-06 18:52:23 +0530

    button util

---
+++ src/components/utils/index.js
@@ -0,0 +1,25 @@
+import { Button } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const ContainedButton = styled((props) => <Button {...props} />)(({ theme }) => ({
+  marginTop: '10px',
+  padding: '6px 30px',
+  fontSize: 14,
+  display: 'flex',
+  alignItems: 'center',
+  outline: 'none',
+  border: '2px solid #293934',
+  borderRadius: '50px',
+  backgroundColor: '#293934',
+  textTransform: 'Capitalize',
+  color: '#f2f2f2',
+  transition: 'all 0.3s ease-in-out',
+  fontWeight: 500,
+  '&:hover': {
+    color: '#293934da',
+    backgroundColor: 'transparent',
+    border: '2px solid #293934',
+  }
+}));
+
+export default ContainedButton;
